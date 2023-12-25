f9c792442fd21c7023dcc8a6b8893299e9787405
Kevin Wang <kevwa@microsoft.com>
2023-12-25 09:25:37 +0800

    changes

---
+++ Day25/Day25.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2023.Day25
+{
+    internal class Day25 : Day
+    {
+        internal override string GetPartOneAnswer(IEnumerable<string> input)
+        {
+            return "Not implemented";
+        }
+
+        internal override string GetPartTwoAnswer(IEnumerable<string> input)
+        {
+            return "Not implemented";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ using AdventOfCode2023;
 var types = Assembly.GetExecutingAssembly().GetTypes();
 foreach (var type in types)
 {
-    if (typeof(Day).IsAssignableFrom(type) && !(type.IsAbstract))
+    if (typeof(Day).IsAssignableFrom(type) && !type.IsAbstract)
     {
         var ctor = type.GetConstructor(Type.EmptyTypes);
         if (ctor != null)
         {
-            var day = (Day)ctor.Invoke(null);
+            var day = (Day)ctor.Invoke(default);
             day.PrintAnswers();
         }
     }
